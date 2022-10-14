8447cda3bf11fd8df3d1ddb0b8a372b14e56a32a
competitiverival <wedsdsa22@gmail.com>
2022-10-14 17:32:48 +0500

    app accounts

--- FilmsCorp/settings.py
+++ FilmsCorp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'films',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#authencation of user
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+
+
+
+
+
+
+
+
+
+
