0d1b844f9e8d61597ba6c25a22980acee1567d11
Competitiverival <92662013+Racconisprogrammer@users.noreply.github.com>
2022-10-28 09:43:06 -0700

    CC@

--- films/urls.py
+++ films/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('post/<int:pk>/', HomePageViewDetail.as_view(), name='post_detail'),
     path('post/update/<int:pk>/', UpdatePageView.as_view(), name='update_post'),
-    path('post/delete/<int:pk>/', DeletePageView.as_view(), name='delete_post'),
+    path('post/delete  /<int:pk>/', DeletePageView.as_view(), name='delete_post'),
     path('post/new/', CreatePageView.as_view(), name='create_post'),
 ]
